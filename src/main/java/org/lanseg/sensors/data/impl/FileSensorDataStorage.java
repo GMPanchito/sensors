d3d30ac68a2da9ddfd8dedf5d1efaf4ab2fe95a0
Andrew Rusakov <lans.rf@gmail.com>
2014-04-10 07:52:41 +0400

    Added sample backend

--- src/main/java/org/lanseg/sensors/data/impl/FileSensorDataStorage.java
+++ src/main/java/org/lanseg/sensors/data/impl/FileSensorDataStorage.java
@@ -2,7 +2,6 @@ package org.lanseg.sensors.data.impl;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
